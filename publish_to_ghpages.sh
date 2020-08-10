e04d65692d20cfc47cd06894bacd28bd4625e486
skykui <skykui@gmail.com>
2020-08-10 14:51:29 +0800

    update publish script [skip travis]

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -24,7 +24,7 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh) [skip travis]"
 
 #echo "Pushing to github"
 #git push --all
